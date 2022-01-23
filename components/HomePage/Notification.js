9e8b26886cd38c7f12d66f655b0e31f1d6c22f88
Nam <hoangnam12c12@gmail.com>
2022-01-23 15:48:26 +0700

    testing manipulation on map

--- components/HomePage/Notification.js
+++ components/HomePage/Notification.js
@@ -8,34 +8,30 @@ import {
   Platform,
   StatusBar,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import {NavigationBar} from '../Menu/NavigationBar';
 import Geolocation from 'react-native-geolocation-service';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import Geocoder from 'react-native-geocoding';
+import axios from 'axios';
+import {GEOCODING_API} from '../../global';
 
 Geocoder.init('AIzaSyDRXvYbjscujWed7pBPKRGCIsmx922HTJI');
 
 export const Notification = props => {
-  const [lat, setLat] = useState(0);
-  const [long, setLong] = useState(0);
+  const [location, setLocation] = useState({latitude: 0, longitude: 0});
   const [loading, setLoading] = useState(true);
-
-  let mapviewRef = useRef();
-
-  const GetLocationPermission = async () => {
-    if (Platform.OS === 'ios') {
-      let permission = await Geolocation.requestAuthorization('whenInUse');
-      console.log(permission);
-    }
-  };
+  const [address, setAddress] = useState('Road');
 
   const getGeolocation = () => {
-    Geolocation.watchPosition(
+    Geolocation.getCurrentPosition(
       position => {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
       },
       err => {
         alert(err.message);
@@ -44,31 +40,36 @@ export const Notification = props => {
     );
   };
 
-  const onClickLocation = event => {
-    // console.log(event.nativeEvent.coordinate);
-    setLat(event.nativeEvent.coordinate.latitude);
-    setLong(event.nativeEvent.coordinate.longitude);
+  const getAddressString = async event => {
+    console.log('New location', event.nativeEvent.coordinate);
+
+    let res = await axios.get(
+      `https://api.opencagedata.com/geocode/v1/json?q=${event.nativeEvent.coordinate.latitude}+${event.nativeEvent.coordinate.longitude}&key=${GEOCODING_API}`,
+    );
+    if (res.data.status.code === 200) {
+      console.log('success', res.data.results[0]);
+      setAddress(res.data.results[0].formatted);
+    }
   };
 
-  const getAddressString = async event => {
-    // let result = await mapviewRef.current.addressForCoordinate({
-    //   latitude: event.nativeEvent.coordinate.latitude,
-    //   longitude: event.nativeEvent.coordinate.longitude,
-    // });
-    // console.log(result);
-    mapviewRef.current
-      .addressForCoordinate({
-        latitude: 12.203214000000004,
-        longitude: 109.19345021534353,
-      })
-      .then(address => console.log(address));
+  const focusToLocation = () => {
+    setLocation({latitude: 12.203214000000004, longitude: 109.19345021534353});
+  };
+
+  const onRegionChange = event => {
+    setLocation({
+      latitude: event.latitude,
+      longitude: event.longitude,
+    });
   };
 
   useEffect(() => {
     setTimeout(async () => {
       if (Platform.OS === 'ios') {
-        await GetLocationPermission();
-        getGeolocation();
+        let permission = await Geolocation.requestAuthorization('whenInUse');
+        if (permission === 'granted') {
+          getGeolocation();
+        }
       } else {
         try {
           const granted = await PermissionsAndroid.request(
@@ -88,14 +89,9 @@ export const Notification = props => {
           console.warn(err);
         }
       }
-      console.log('Lat and long: ' + lat + ' ' + long);
-      // Geocoder.from('Colosseum')
-      //   .then(json => {
-      //     var location = json.results[0].geometry.location;
-      //     console.log(location);
-      //   })
-      //   .catch(error => console.warn(error));
-      // setLoading(false);
+      console.log(
+        'Lat and long: ' + location.latitude + ' ' + location.longitude,
+      );
     }, 200);
     setTimeout(() => {
       setLoading(false);
@@ -109,7 +105,7 @@ export const Notification = props => {
   if (loading) {
     return (
       <View style={styles.container}>
-        <View style={styles.container}>
+        <View style={[styles.container, {backgroundColor: 'transparent'}]}>
           <StatusBar barStyle="dark-content" />
           <ActivityIndicator size={'large'} color={'red'} />
         </View>
@@ -125,30 +121,39 @@ export const Notification = props => {
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.map}
           minZoomLevel={17}
-          // onPress={onClickLocation}
           onPress={event => getAddressString(event)}
           showsUserLocation
-          region={{
-            latitude: lat,
-            longitude: long,
+          initialRegion={{
+            latitude: 12.203214000000004,
+            longitude: 109.19345021534353,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
-          }}>
+          }}
+          region={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          // onRegionChange={event => onRegionChange(event)}
+        >
           {/* <Marker
-            coordinate={{latitude: lat, longitude: long}}
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
             title="You are here"
             description="This is your location"
-            draggable
-            onDragEnd={e =>
-              console.log(JSON.stringify(e.nativeEvent.coordinate))
-            }
           /> */}
-          <TouchableOpacity
-            style={styles.currentPositionButton}
-            onPress={() => getGeolocation()}>
-            <Feather name="navigation" size={20} color={'black'} />
-          </TouchableOpacity>
         </MapView>
+        <TouchableOpacity
+          style={styles.currentPositionButton}
+          onPress={() => focusToLocation()}>
+          <Feather name="navigation" size={20} color={'black'} />
+        </TouchableOpacity>
+        <View style={styles.inputWrapper}>
+          <TextInput style={styles.inputField} value={address} editable />
+        </View>
       </View>
       <NavigationBar active={props.tabname} />
     </View>
@@ -169,10 +174,28 @@ const styles = StyleSheet.create({
   currentPositionButton: {
     position: 'absolute',
     top: '80%',
-    left: '89%',
+    left: '85%',
     padding: 10,
     borderRadius: 20,
     backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#c6c6c6',
+  },
+  inputWrapper: {
+    position: 'absolute',
+    top: '10%',
+    left: '0%',
+    right: 0,
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  inputField: {
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
 
